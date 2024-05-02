56caa12cc6b49b2a9ed66be8131f18337f3f5046
Superman <hsbtiago@live.com>
2024-05-01 20:51:32 -0400

    update net8

--- App/Data/ConnectionHelper.cs
+++ App/Data/ConnectionHelper.cs
@@ -22,8 +22,7 @@ public static class ConnectionHelper
             Username = userInfo[0],
             Password = userInfo[1],
             Database = databaseUri.LocalPath.TrimStart('/'),
-            SslMode = SslMode.Require,
-            TrustServerCertificate = true
+            SslMode = SslMode.Require
         };
         return builder.ToString();
     }
